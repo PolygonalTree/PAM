d7f5e74d7737496c5578da3925cff8fe399c2308
PolygonalTree <luis.garcia@polygonaltree.co.uk>
2015-07-26 21:21:33 +0100

    fist commit

--- js/app.js
+++
@@ -1,7 +0,0 @@
-
-var pamApp = angular.module('pamApp',[]);
-
-pamApp.controller("mainController", function($scope){
-    $scope.listOfApps = management.listOfApps();
-});
-
--- scripts/App_launcher.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-export APP_NAME="${1}"
-
-echo "starting App"
-source "$WORKON_HOME/$APP_NAME/bin/activate"
-cd "$PROJECT_HOME/$APP_NAME/bin/"
-
-python run.py
