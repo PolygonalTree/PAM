3c8019486e49a2514af9e1f7a3c0133afffa80c3
PolygonalTree <luis.garcia@polygonaltree.co.uk>
2015-07-29 01:09:25 +0100

    feedback to user added

--- js/main.js
+++ js/main.js
@@ -9,19 +9,65 @@ var logger = new Console(output, errorOutput);
 
 var pamApp = angular.module('pamApp',[]);
 
-var lastView = "applicationList"
+if (sessionStorage.lastView == null){
+    sessionStorage.lastView = "applicationList";
+    sessionStorage.isInstalling =false;
+}
+
+
+
+
 
 pamApp.controller("mainController", function($scope, $timeout){
     $scope.manager = {};
     $scope.install = {};
+    $scope.manager.output="";
     $(".button-collapse").sideNav();
     $scope.manager.runningApps = {};
-    $scope.manager.show=lastView;
-    $scope.manager.output ="";
+    $scope.manager.show=sessionStorage.lastView;
     $scope.install.pythonVersion="python3";
-
     $scope.manager.listOfApps = management.listOfApps();
 
+    $scope.manager.check = function(){
+        outfile=fs.readFileSync('./out.log');
+        outfile=String(outfile);
+        if (outfile.search("Installation exited with code")>0){
+            $scope.manager.output="Error on launch";
+            $timeout(function(){$scope.manager.output=""},5000);
+        }else if(outfile.search("Launch completed")>0){
+            fs.writeFile('./out.log', '', function(){console.log('done')});
+            $scope.manager.output="Application Closed";
+            $scope.$apply();
+       };
+    }
+
+    $scope.manager.checkInstall = function(){
+        console.log("CALLED");
+        outfile=fs.readFileSync('./out.log');
+        outfile=String(outfile);
+        if (outfile.search("Instalation_complete")>0){
+            $scope.manager.output="Instalation Completed";
+
+            console.log($scope.manager.output);
+            fs.writeFile('./out.log', '', function(){console.log('done'); });
+            $scope.manager.show="applicationList";
+            sessionStorage.lastView="applicationList";
+            $timeout.cancel(checking);
+            $scope.$apply();
+
+        }else if(outfile.search("exited with code")>0){
+            $scope.manager.output="Instalation Error for more info check out.log";
+            $timeout.cancel(checking);
+        }else if (sessionStorage.isInstalling){
+            checking=$timeout($scope.manager.checkInstall,3000);
+        };
+    }
+
+    if (sessionStorage.isInstalling){
+        console.log("Installing");
+        $scope.manager.checkInstall();
+        checking=$timeout($scope.manager.checkInstall,1000);
+    }
 
     $scope.install.fileNameChanged = function (name){
        //TODO:check that file is valid
@@ -33,40 +79,37 @@ pamApp.controller("mainController", function($scope, $timeout){
     }
 
     $scope.install.performInstallation = function(){
-        lastView="installationProcess";
-        $scope.manager.show = "installationProcess";
+        $scope.manager.show="installProcess";
+        sessionStorage.lastView="installProcess";
+        sessionStorage.isInstalling =true;
+        if (sessionStorage.isInstalling == true){
+        checkInstall();
+        }
         var path = $scope.AppFile;
         var file=$scope.AppFile.split('/');
         var app=file[file.length-1];
         var appSplit = app.split('.');
         var AppName = appSplit[0];
         var AppExtension = appSplit.slice(1,appSplit.length).join('');
-
         instProcess = management.install(path,AppName,$scope.install.pythonVersion);
-        instProcess.on('close', function (code) {
-        if (code !== 0) {
-            console.log('Installation exited with code ' + code);
-            logger.log('Installation exited with code ' + code);
-        }else{
-            logger.log('Instalation complete');
-            console.log('Instalation complete');
+
+        instProcessClose = instProcess.on('close', function(code){
+                if (code !== 0) {
+                    console.log('Installation exited with code ' + code);
+                    logger.log('Installation exited with code ' + code);
+                }else{
+                    logger.log('Instalation complete');
+                    console.log('Instalation complete');
+                    return true;
+                }
+
+                });
+        if (sessionStorage.isInstalling == true){
+            checking = $timeout(checkInstall(),1000);
         }
-            $timeout(checkInstall,2500);
-        });
-
-     }
-
-    checkInstall = function(){
-        outfile=fs.readFileSync('./out.log');
-        outfile=String(outfile);
-        if (outfile.search("Instalation_complete")>0){
-            $scope.manager.output="Instalation Complete";
-            //$timeout(function(){$scope.manager.output=""},5000);
-        }else{
-            $scope.manager.output="Instalation Error for more info check out.log";
-            //$timeout(function(){$scope.manager.output=""},5000);
-        };
     }
+
+
 
     $scope.launchApp = function(appName){
         console.log("launching app "+appName);
@@ -88,26 +131,15 @@ pamApp.controller("mainController", function($scope, $timeout){
                 console.log('Launch exited with code ' + code);
                 logger.log('Launch exited with code ' + code);
             }else{
-                console.log('Launch complete');
-                logger.log('Launch complete');
+                console.log('Launch completed');
+                logger.log('Launch completed');
             }
-            $timeout(check,1000);
+            $scope.manager.check();
         });
 
     }
-    check = function(){
-        outfile=fs.readFileSync('./out.log');
-        outfile=String(outfile);
-        if (outfile.search("Installation exited with code")>0){
-            $scope.manager.output="Error on launch";
-            $timeout(function(){$scope.manager.output=""},5000);
-        }else if(outfile.search("Launch complete")>0){
-            fs.writeFile('./out.log', '', function(){console.log('done')})
-            $scope.manager.output="Ended";
-        }else{
-         $timeout(check,1000);
-        };
-    }
+
+
     $scope.stopApp = function(appName){
         $scope.manager.runningApps.appName.kill('SIGHUP');
         delete runningApps.appName;
@@ -117,5 +149,8 @@ pamApp.controller("mainController", function($scope, $timeout){
 
 management.initWorkspace();
 console.log("Initialised");
+
+
+
 })();
 
